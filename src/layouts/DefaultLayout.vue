bb6e0614701ce0a5ce221901443e760d8cc59534
Marksiranat <siranat2546@gmail.com>
2025-06-12 14:44:30 +0700

    dev_mark_Navbar

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-toolbar height="80" color="indigo-lighten-1" dark>
+      <v-toolbar-title class="text-h6">Toolbar</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn icon>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <v-footer
+      class="text-center d-flex flex-column ga-1 py-2"
+      color="indigo-lighten-3"
+      height="20px"
+    >
+      <div class="d-flex ga-2">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          :icon="icon"
+          density="compact"
+          size="24"
+          variant="text"
+        />
+      </div>
+
+      <v-divider class="my-1" thickness="1" width="40"></v-divider>
+      <v-divider />
+    </v-footer>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const icons = ref(["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"]);
+</script>
--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -1,19 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from '@/plugins/stores/auth' // นำเข้า store
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
+      path: "/about",
+      name: "about",
+      component: () => import("@/views/AboutView.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue'),
+      path: "/ProductView",
+      name: "productview",
+      component: () => import("@/views/ProductView.vue"),
+    },
+
+    {
+      path: "/",
+      name: "defaultlayout",
+      component: () => import("@/layouts/DefaultLayout.vue"),
+      children: [
+        {
+          path: "home",
+          name: "home",
+          component: () => import("@/views/HomeView.vue"),
+          meta: { requiresAuth: false },
+        },
+      ],
     },
   ],
+});
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore()
+  if (to.meta.requiresAuth && !auth.isLoggedIn) {
+    next({ name: 'productview' }) // ถ้าไม่ได้ login ให้กลับไปหน้า home
+  } else {
+    next() // ปล่อยให้ไปได้
+  }
 })
 
-export default router
+export default router;
