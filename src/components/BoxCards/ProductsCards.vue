1fef257288e6f1ff762b8c36ba9fbf56a50c1065
Marksiranat <siranat2546@gmail.com>
2025-06-12 17:01:00 +0700

    dev_mark

--- src/components/BoxCards/ProductsCards.vue
+++ src/components/BoxCards/ProductsCards.vue
@@ -1,33 +1,19 @@
 <template>
   <v-row dense>
-    <v-col
-      v-for="product in products"
-      :key="product.id"
-      cols="12"
-      sm="6"
-      md="4"
-      lg="3"
-    >
-      <v-card
-        class="mx-auto"
-        max-width="344"
-        hover
-        @click="goToDetail(product.id)"
-      >
-        <v-img
-          :src="product.image"
-          height="200px"
-          cover
-        ></v-img>
+    <v-col v-for="product in products" :key="product.id" cols="12" sm="6" md="4" lg="3">
+      <v-card class="mx-auto" max-width="450" hover @click="goToDetail(product.id)">
+        <v-img :src="product.image" height="200px" cover></v-img>
 
-        <v-card-title class="text-h6">{{ product.name }}</v-card-title>
+        <v-card-title class="text-h6">{{ product.description }}</v-card-title>
 
-        <v-card-subtitle class="text-subtitle-2 text-primary">
+        <v-card-subtitle class="text-subtitle-2 text-black text-h7 ">
           ฿{{ product.price.toLocaleString() }}
         </v-card-subtitle>
 
-        <v-card-text class="text-truncate">
-          {{ product.description }}
+        <v-card-text class="d-flex justify-end">
+          <v-btn color="indigo-lighten-1" @click="goToDetail(product.id)" class="rounded-pill">
+            รายละเอียด
+          </v-btn>
         </v-card-text>
       </v-card>
     </v-col>
@@ -55,7 +41,7 @@ function goToDetail(id: number) {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 2; /* Limit to 2 lines */
+  -webkit-line-clamp: 3; /* Limit to 2 lines */
   -webkit-box-orient: vertical;
 }
 </style>
